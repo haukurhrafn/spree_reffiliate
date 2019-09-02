fcfb1240bb6a9fb411d6849f13c2ddfa8074e130
Haukur Thorsteinsson <haukur@haukur.net>
2019-09-02 17:14:44 +0000

    Upgrade version to support spree 3.2

--- lib/spree_reffiliate/version.rb
+++ lib/spree_reffiliate/version.rb
@@ -9,8 +9,8 @@ module SpreeReffiliate
 
   module VERSION
     MAJOR = 3
-    MINOR = 1
-    TINY  = 1
+    MINOR = 7
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
